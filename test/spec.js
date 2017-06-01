a922b84589b0af258158069bc1680c81a3bfa141
Gilad Peleg <giladp007@gmail.com>
2017-06-01 16:52:51 +0300

    Refactor and create build for UMD, ES5, ES6 (#3)

--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,8 @@
-import './setup';
-
 import React from 'react';
 import test from 'tape';
 import { shallow } from 'enzyme';
 
-import PageVisibility from '../index';
+import PageVisibility from '../src/index';
 
 const noop = function () {};
 
