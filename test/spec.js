59b00b78c188b589882175000dcde3b94e357880
Gilad Peleg <giladp007@gmail.com>
2019-02-13 10:29:11 +0200

    Update tests

--- test/spec.js
+++ test/spec.js
@@ -1,56 +1,55 @@
 import React from 'react';
 import sinon from 'sinon';
+import {shallow} from 'enzyme';
 import test from 'tape';
-import { shallow } from 'enzyme';
 
-import PageVisibility  from '../src/index';
+import PageVisibility from '../src/index';
 
-const noop = function () {};
+const noop = function () {
+};
 
 const Child = props => {
-    const { documentHidden, visibilityState } = props;
+    const {documentHidden, visibilityState} = props;
     return <div>
         <p>{documentHidden ? 'hidden' : 'shown'}</p>
         <p>{visibilityState}</p>
     </div>;
 };
 
-test('PageVisibility component', t => {
-    t.test('render the component', t => {
-        const result = shallow(<PageVisibility onChange={noop} />);
-        t.equal(result.length, 1);
-        t.end();
-    });
+test('render the component', t => {
+    const result = shallow(<PageVisibility onChange={noop}/>);
+    t.equal(result.length, 1);
+    t.end();
+});
 
-    t.test('throw if trying to render multiple direct children', t => {
-        t.throws(() => shallow(
-            <PageVisibility onChange={noop}>
-                <div />
-                <div />
-            </PageVisibility>
-        ), /Invariant Violation/);
-        t.end();
-    });
+test('throw if trying to render multiple direct children', t => {
+    t.throws(() => shallow(
+        <PageVisibility onChange={noop}>
+            <div/>
+            <div/>
+        </PageVisibility>
+    ), /Invariant Violation/);
+    t.end();
+});
 
-    t.test('throw if trying to render multiple custom direct children', t => {
-        t.throws(() => shallow(
-            <PageVisibility onChange={noop}>
-                <Child />
-                <Child />
-            </PageVisibility>
-        ), /Invariant Violation/);
-        t.end();
-    });
+test('throw if trying to render multiple custom direct children', t => {
+    t.throws(() => shallow(
+        <PageVisibility onChange={noop}>
+            <Child/>
+            <Child/>
+        </PageVisibility>
+    ), /Invariant Violation/);
+    t.end();
+});
 
-    t.test('allow children as function', t => {
-        const stub = sinon.stub();
-        const wrapper = shallow(<PageVisibility>{stub}</PageVisibility>);
+test('allow children as function', t => {
+    const stub = sinon.stub();
+    const wrapper = shallow(<PageVisibility>{stub}</PageVisibility>);
 
-        t.equal(wrapper.length, 1);
-        t.equal(stub.callCount, 1);
-        t.equal(stub.firstCall.args.length, 2);
-        t.looseEquals(stub.firstCall.args, [false, 'prerender']);
+    t.equal(wrapper.length, 1);
+    t.equal(stub.callCount, 1);
+    t.equal(stub.firstCall.args.length, 2);
+    t.looseEquals(stub.firstCall.args, [false, 'prerender']);
 
-        t.end();
-    });
+    t.end();
 });
